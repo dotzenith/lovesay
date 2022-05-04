c14b9576b5626b77e22dd5cd2175d762e5aafde4
jeremy bolster <jeremy.bolster@salesforce.com>
2022-05-04 15:33:39 -0700

    Add the ability to specify a max width
    On large terminals it is desireable to have shorter output for
    easier reading.

--- lovesay/cli.py
+++ lovesay/cli.py
@@ -1,18 +1,20 @@
 from typing import Optional
 import typer
 import lovesay.love as lovesay
+import shutil
 
 app = typer.Typer(add_completion=False)
 
 @app.command()
 def love(
-    quote: Optional[str] = typer.Argument(None, help="An arbitrary message to print", show_default=False), 
-    color: str = typer.Option('catppuccin', "--color", "-c", help="The name of your preferred color scheme")        
+    quote: Optional[str] = typer.Argument(None, help="An arbitrary message to print", show_default=False),
+    color: str = typer.Option('catppuccin', "--color", "-c", help="The name of your preferred color scheme"),
+    max_width: int = typer.Option(shutil.get_terminal_size()[0], "--max-width", help="Set a max width for quotes, must be less than the terminal can fit.")
 ):
     """
     Supported color schemes: catppuccin, dracula, nord, gruvbox, onedark, tokyonight, rosepine, ayu, palenight, and gogh 
     """
-    lovesay.main(quote, color)
+    lovesay.main(quote, color, max_width)
 
 def main():
     app()
