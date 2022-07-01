169a5e946cd12d83385ee963a734e8ed32c01494
dotzenith <sharmadanshu01@gmail.com>
2022-07-01 00:17:30 -0400

    switched to kolorz for colors

--- lovesay/cli.py
+++ lovesay/cli.py
@@ -8,11 +8,11 @@ app = typer.Typer(add_completion=False)
 @app.command()
 def love(
     quote: Optional[str] = typer.Argument(None, help="An arbitrary message to print", show_default=False),
-    color: str = typer.Option('catppuccin', "--color", "-c", help="The name of your preferred color scheme"),
+    color: str = typer.Option('catppuccin mocha', "--color", "-c", help="The name of your preferred color scheme"),
     max_width: int = typer.Option(shutil.get_terminal_size()[0], "--max-width", help="Set a max width for quotes, must be less than the terminal can fit.")
 ):
     """
-    Supported color schemes: catppuccin, dracula, nord, gruvbox, onedark, tokyonight, rosepine, ayu, palenight, and gogh 
+    Supported color schemes: catppuccin [latte, frappe, macchiato, mocha], dracula, nord, gruvbox, onedark, tokyonight, ayu, palenight, and gogh 
     """
     lovesay.main(quote, color, max_width)
 
